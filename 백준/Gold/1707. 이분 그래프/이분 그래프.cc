8998fa57f1a04b28d2c67e098317fd713ac5f524
dosawas <87902719+Dosawasseungjun@users.noreply.github.com>
2024-05-20 01:03:13 +0900

    [Gold IV] Title: 이분 그래프, Time: 248 ms, Memory: 7228 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.cc
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.cc
@@ -1,63 +1,51 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <cstring>
-
+#include <bits/stdc++.h>
+#define fast_io cin.tie(NULL); ios_base::sync_with_stdio(false);
 using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tuple<int, int, int> tiii;
+typedef tuple<ll, ll, ll> tlll;
+#define xx first
+#define yy second
 
-vector<vector<int>> adj; //인접 리스트 표현
-bool visited[2][40001]; //짝수번째 방문했는지 홀수번째 방문했는지 판단
-void bfs(int start, int cnt){
-    
-    queue<pair<int, int>> q;
-    visited[cnt%2][start] = true;
-    q.push(make_pair(cnt, start));
-    
-    while (!q.empty()) {
-        pair<int, int> here = q.front();
-        q.pop();
-        for(int i=0;i<adj[here.second].size();i++){
-            if(!visited[(here.first+1)%2][adj[here.second][i]]){
-                q.push(make_pair(here.first+1, adj[here.second][i]));
-                visited[(here.first+1)%2][adj[here.second][i]] = true;
-            }
-        }
+const int V_MAX = 2e4+1;
+vector<vector<int>> adj;
+int vst[V_MAX][2];
+
+void dfs(int here, int cnt){
+    vst[here][cnt] = true;
+    for(int nxt : adj[here]){
+        if(vst[nxt][cnt ^ 1]) continue;
+        dfs(nxt, cnt ^ 1);
     }
-    
 }
 
-void bfsAll(){
-    for(int i=1;i<adj.size();i++){
-        if(!visited[0][i]&&!visited[1][i]){
-            bfs(i,0);
-        }
+void dfsAll(int v){
+    for(int i=1;i<=v;i++) if(!vst[i][0] && !vst[i][1]) dfs(i, 0);
+}
+
+void solv(){
+    int v, e; cin >> v >> e;
+    adj = vector(v+1, vector<int>());
+    memset(vst, 0, sizeof(vst));
+    for(int i=0;i<e;i++){
+        int u, v; cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
+    dfsAll(v);
+    bool isBipar = true;
+    for(int i=1;i<=v;i++){
+        if(vst[i][0] + vst[i][1] == 1) continue;
+        isBipar = false;
+    }
+    if(isBipar) cout << "YES\n";
+    else cout << "NO\n";
 }
 
 int main(){
-    int test;
-    cin >> test;
-    
-    while (test--) {
-        int v,e;
-        cin >> v >> e;
-        
-        memset(visited, false, sizeof(visited));
-        adj = vector<vector<int>> (v+1);
-        
-        for(int i=0;i<e;i++){
-            int a, b;
-            cin >> a >> b;
-            adj[a].push_back(b);
-            adj[b].push_back(a);
-        }
-        
-        bfsAll();
-        bool result = true;
-        for(int i=1;i<=v;i++){
-            if(visited[0][i]&&visited[1][i]) result = false;
-        }
-        if(result) cout << "YES" << '\n';
-        else cout << "NO" << '\n';
-    }
+    fast_io
+    int tt; cin >> tt;
+    while(tt--) solv();
 }
