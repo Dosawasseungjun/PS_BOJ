5508bbd830ae77d6b72777bad7d82efb00ec92d5
dosawas <87902719+Dosawasseungjun@users.noreply.github.com>
2024-07-20 19:17:55 +0900

    [Gold IV] Title: 어려운 소인수분해, Time: 812 ms, Memory: 48032 KB -BaekjoonHub

--- 백준/Gold/16563. 어려운 소인수분해/어려운 소인수분해.cc
+++ 백준/Gold/16563. 어려운 소인수분해/어려운 소인수분해.cc
@@ -4,32 +4,113 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
-typedef tuple<int, int, int > tiii;
+typedef tuple<int, int, int> tiii;
+typedef tuple<ll, ll, ll> tlll;
+#define xx first
+#define yy second
 
-const int MAX = 5e6+1;
-ll P[MAX];
+vector<ll> eratos(int n){ // n은 1이상의 자연수 
+    vector<ll> table(n+1, 0), primes;
+    // table[i] = i면 소수
+    // table[i] != i면 합성수고 i를 인수로 가짐
+    for(ll i = 2 ; i <= n; i++){
+        if(table[i]) continue;
+        table[i] = i; // 색칠하고
+        primes.push_back(table[i]);
+        for(ll j = i * i; j < n; j += i){
+            table[i] = i;
+        }
+    }
+    return primes;
+    // return table;
+}
 
+template <int SZ>
+struct NumTheory{
+    vector<ll> primes;
+    ll table[SZ + 1];
+    vector<ll> eratos(int sz = SZ){
+        table[1] = 1; // 소인수분해 할때만 1로 해둡시다.예외로
+        for(ll i = 2;i<sz;i++){
+            if(table[i]) continue;
+            table[i] = i;
+            primes.push_back(i);
+            for(ll j = i * i; j < sz ; j += i){
+                table[j] = i;
+            }
+        }
+        return primes;
+    }
+    vector<int> factorization(int n){
+        vector<int> ret;
+        while(n != 1){
+            ret.push_back(table[n]);
+            n /= table[n];
+        }
+        // ret.push_back(1);
+        sort(ret.begin(), ret.end());
+        return ret;
+    }
+};
+
+template <int SZ, ll MOD>
+struct PrimeControl
+{
+    vector<ll> P;
+    ll p[SZ + 1];
+    // p[i] = i면 소수 - 물론 1은 제외
+    // P[i] != i면 합성수고 i를 인수로 가짐
+    void eratos(int sz = SZ)
+    {
+        p[1] = 1;
+        for (ll i = 2; i < sz; i++)
+        {
+            if (p[i])
+                continue;
+            p[i] = i;
+            P.push_back(i);
+            for (ll j = i * i; j < sz; j += i)
+                p[j] = i;
+        }
+    }
+    // n을 소인수 분해
+    vector<int> factorization(int n)
+    {
+        vector<int> ret;
+        while (n != 1)
+        {
+            ret.push_back(p[n]);
+            n /= p[n];
+        }
+        return ret;
+    }
+};
+
+vector<ll> factors(int x){ // x를 소인수분해
+    vector<ll> f;
+    for(ll i = 1; i * i<= x; i++){
+        if(x % i == 0){
+            ll a = i;
+            ll b = x / i;
+            if(a != b) f.push_back(a);
+            f.push_back(b);
+        }
+    }
+    return f;
+}
+
+const int MAX = 5e6;
+NumTheory<MAX> nt; // table 용량땜에 전역변수로 했음
 int main(){
     fast_io
     int n; cin >> n;
-    P[1] = 1;
-    for(ll i=2;i<MAX;i++){
-        if(P[i]) continue;
-        P[i] = i;
-        for(ll j=i*i;j<MAX;j+=i){
-            if(!P[j]) P[j] = i;
-        }
-    }
+    nt.eratos();
     for(int i=0;i<n;i++){
         int k; cin >> k;
-        vector<int> res;
-        while(k!=1){
-            res.push_back(P[k]);
-            k /= P[k];
+        vector<int> factors = nt.factorization(k);
+        for(int f : factors){
+            cout << f << ' ';
         }
-        
-        sort(res.begin(), res.end());
-        for(int j=0;j<res.size();j++) cout << res[j] << ' ';
         cout << '\n';
     }
 }
