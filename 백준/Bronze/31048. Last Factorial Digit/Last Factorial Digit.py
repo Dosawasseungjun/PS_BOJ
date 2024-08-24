425ea68e37499ff948a663386d94ddb5641e194b
dosawas <87902719+Dosawasseungjun@users.noreply.github.com>
2024-08-24 17:18:35 +0900

    [Bronze IV] Title: Last Factorial Digit, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/31048. Last Factorial Digit/Last Factorial Digit.py
@@ -0,0 +1,7 @@
+for i in range(int(input())) : 
+    N = int(input())
+    if N == 0 or N == 1 : print(1)
+    elif N == 2 : print(2)
+    elif N == 3 : print(6)
+    elif N == 4 : print(4)
+    else : print(0)
