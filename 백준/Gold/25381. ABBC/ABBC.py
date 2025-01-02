5d62470f74356334a2f825ff14dcd94bb5afc227
dosawas <87902719+Dosawasseungjun@users.noreply.github.com>
2025-01-02 20:43:19 +0900

    [Gold III] Title: ABBC, Time: 60 ms, Memory: 35084 KB, Score: 52 point -BaekjoonHub

--- 백준/Gold/25381. ABBC/ABBC.py
+++ 백준/Gold/25381. ABBC/ABBC.py
@@ -1,16 +1,23 @@
 S = input()
-A, B, C, R = 0, 0, 0, 0
-for c in S[::-1] :
-    if c == 'A' : 
-        if B : 
-            B -= 1
-            R += 1
-    elif c == 'B' : 
-        if C : 
-            C -= 1
-            R += 1
-        else : 
-            B += 1
-    else : 
-        C += 1
+N = len(S)
+V = [0]*N
+R = 0
+from collections import deque
+dq = deque()
+for i in range(N) : 
+    if S[i] == 'B' : dq.append(i)
+    elif S[i] == 'C' and dq: 
+        R += 1
+        V[dq.popleft()] = 1
+        V[i] = 1
+T = ""
+for i in range(N) : 
+    if not V[i] : T += S[i]
+M = len(T)
+dq.clear()
+for i in range(M) : 
+    if T[i] == 'A' : dq.append(i)
+    elif T[i] == 'B' and dq: 
+        R += 1
+        dq.popleft()
 print(R)
