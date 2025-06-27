7f23dad72782bf09d4e2122827553975b13e8a69
dosawas <87902719+Dosawasseungjun@users.noreply.github.com>
2025-06-27 15:09:54 +0900

    [Gold III] Title: 서로소 그래프 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/34035. 서로소 그래프 게임/서로소 그래프 게임.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define fast_io cin.tie(NULL); ios_base::sync_with_stdio(false);
+using namespace std;
+
+void solv(){
+    int N; cin >> N;
+    cout << (N == 2 ? "kang\n" : "chan\n");
+}
+
+int main(){
+    fast_io
+    int tt; cin >> tt;
+    while(tt--) solv();
+}
