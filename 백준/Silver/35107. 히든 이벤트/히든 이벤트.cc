5c83c699bc109ffda3b9e1998a8e5770872ca985
dosawas <87902719+Dosawasseungjun@users.noreply.github.com>
2026-01-18 14:31:36 +0900

    [Silver II] Title: 히든 이벤트, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/35107. 히든 이벤트/히든 이벤트.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define fast_io cin.tie(NULL); ios_base::sync_with_stdio(false);
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tuple<int, int, int> tiii;
+typedef tuple<ll, ll, ll> tlll;
+#define xx first
+#define yy second
+
+
+int main(){
+    fast_io
+    int n, m, k; cin >> n >> m >> k;
+    double p = n-m, q = n;
+    double x = 1;
+    cout << fixed;
+    cout.precision(12);
+    for(int i=0;i<k;i++){
+        x *= p / q;
+        p -= 1; q -= 1;
+        cout << 1 - x << '\n';
+    }
+}
